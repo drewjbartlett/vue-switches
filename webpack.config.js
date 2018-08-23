39ccac4931bbfad3ecab0fbf9eeb50ce7a01fe4c
ezimnitskiy <evgeniy.zimnitskiy@effective-soft.com>
2018-08-23 18:44:27 +0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'vue-switches.min.js',
+    library: 'switches',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = false
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      sourceMap: false
+    }),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ])
+}
